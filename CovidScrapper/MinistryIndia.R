3e6e20433fcedc69dca1d8f8133fc8d94a015ef3
anmoledewan <anmoldew@gmail.com>
2020-03-30 07:56:44 +0530

    initial data commit

---
+++ CovidScrapper/MinistryIndia.R
@@ -0,0 +1,35 @@
+
+library(rvest)
+webpage <- read_html("https://www.mohfw.gov.in/#cases")
+tbls <- html_nodes(webpage, "table")
+tbls_ls <- webpage %>%
+  html_nodes("table") %>%
+  html_table(fill = TRUE)
+tbls_ls
+
+dateinfo=html_nodes(webpage, xpath = "//div[@class='content newtab']")[[1]]
+dateinfo%>%html_nodes("p") %>% html_text()
+
+
+df=data.frame(tbls_ls[[10]])
+indiaData=df
+
+confirmed=indiaData[,c(2,3)]
+names(confirmed)[ncol(confirmed)-1]="States"
+names(confirmed)[ncol(confirmed)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
+confirmed=cbind(read.csv("india_confirmed_covid.csv"),confirmed[,2])
+write.csv(confirmed,file = "india_confirmed_covid.csv")
+
+deaths=indiaData[,c(2,5)]
+names(confirmed)[ncol(confirmed)-1]="States"
+names(confirmed)[ncol(confirmed)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
+confirmed=cbind(read.csv("india_death_covid.csv"),confirmed[,2])
+write.csv(confirmed,file = "india_death_covid.csv")
+
+recovered=indiaData[,c(2,4)]
+names(confirmed)[ncol(confirmed)-1]="States"
+names(confirmed)[ncol(confirmed)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
+confirmed=cbind(read.csv("india_recovered_covid.csv"),confirmed[,2])
+write.csv(confirmed,file = "india_recovered_covid.csv")
+
+S
