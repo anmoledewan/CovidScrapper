5a2b0885253914213ecd18bf92a95e2968d42873
anmoledewan <anmoldew@gmail.com>
2020-03-30 09:17:30 +0530

    clean data

--- CovidScrapper/MinistryIndia.R
+++ CovidScrapper/MinistryIndia.R
@@ -26,8 +26,8 @@ if(mean(confold[,ncol(confold)]==confirmed[,ncol(confirmed)])!=1){
 
 
 deaths=indiaData[,c(2,5)]
-names(confirmed)[ncol(confirmed)-1]="States"
-names(confirmed)[ncol(confirmed)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
+names(deaths)[ncol(deaths)-1]="States"
+names(deaths)[ncol(deaths)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
 confold=read.csv("india_death_covid.csv")
 if(mean(confold[,ncol(confold)]==deaths[,ncol(deaths)])!=1){
 deaths=cbind(read.csv("india_death_covid.csv"),deaths[,2])
@@ -35,8 +35,8 @@ write.csv(deaths,file = "india_death_covid.csv")
 }
 
 recovered=indiaData[,c(2,4)]
-names(confirmed)[ncol(confirmed)-1]="States"
-names(confirmed)[ncol(confirmed)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
+names(recovered)[ncol(recovered)-1]="States"
+names(recovered)[ncol(recovered)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
 confold=read.csv("india_recovered_covid.csv")
 if(mean(confold[,ncol(confold)]==recovered[,ncol(recovered)])!=1){
 recovered=cbind(read.csv("india_recovered_covid.csv"),recovered[,2])
