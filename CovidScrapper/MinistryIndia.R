64abdd51bba4b9aa2c1038399e8b9dc241b8d152
anmoledewan <anmoldew@gmail.com>
2020-04-30 03:27:11 +0530

    29 apr

--- CovidScrapper/MinistryIndia.R
+++ CovidScrapper/MinistryIndia.R
@@ -48,11 +48,17 @@ updateIndiafromMinistry = function(string){
   if(length(y)!=0){
     df=df[-y,]
   }
+  y=  grep("Repeat", df[,2])
+  str(y)
+  if(length(y)!=0){
+    df=df[-y,]
+  }
   
   
   
   indiaData=df
   #indiaData[,2]=as.character(gsub("^[A-z]", "", indiaData[,2]))
+  indiaData[,2]= str_replace_all(indiaData[,2], "[[:punct:]]", "")
   indiaData[,3]=as.numeric(gsub("\\D", "", indiaData[,3]))
   indiaData[,4]=numextract(as.numeric(indiaData[,4]))
   indiaData[,5]=numextract(as.numeric(indiaData[,5]))
@@ -62,41 +68,39 @@ updateIndiafromMinistry = function(string){
   names(confirmed)[ncol(confirmed)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
   confold=read_csv("india_confirmed_covid.csv")[,-1]
   confold
-  if(mean(confold[,ncol(confold)]==confirmed[,ncol(confirmed)])!=1){
+  
     confirmed=merge(x = confold, y = confirmed, by = "States", all.y = TRUE)
     confirmed[is.na(confirmed)] <- 0
     names(confirmed)[ncol(confirmed)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
     #confirmed
     write.csv(confirmed,file = "india_confirmed_covid.csv")
     
-  }
+
   
   
   deaths=indiaData[,c(2,5)]
   names(deaths)[ncol(deaths)-1]="States"
   names(deaths)[ncol(deaths)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
   confold=read_csv("india_death_covid.csv")[,-1]
-  if(mean(confold[,ncol(confold)]==deaths[,ncol(deaths)])!=1){
+  
     
     deaths=merge(x = confold, y = deaths, by = "States", all.y = TRUE)
     deaths[is.na(deaths)] <- 0
     names(deaths)[ncol(deaths)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
     write.csv(deaths,file = "india_death_covid.csv")
     
-  }
+  
   
   recovered=indiaData[,c(2,4)]
   names(recovered)[ncol(recovered)-1]="States"
   names(recovered)[ncol(recovered)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
   confold=read_csv("india_recovered_covid.csv")[,-1]
-  if(mean(confold[,ncol(confold)]==recovered[,ncol(recovered)])!=1){
     
     recovered=merge(x = confold, y = recovered, by = "States", all.y = TRUE)
     recovered[is.na(recovered)] <- 0
     names(recovered)[ncol(recovered)]=trimws(format(as.POSIXlt(Sys.time()),tz = "Asia/Calcutta"), which = c("both"))
     write.csv(recovered,file = "india_recovered_covid.csv")
-    
-  }
+  
   
   
   
