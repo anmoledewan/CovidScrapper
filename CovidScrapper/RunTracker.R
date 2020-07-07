a8dd00be8a64560046c2f78c92df0d401802f330
anmoledewan <anmoldew@gmail.com>
2020-07-08 00:44:33 +0530

    july 3

--- CovidScrapper/RunTracker.R
+++ CovidScrapper/RunTracker.R
@@ -1,5 +1,5 @@
 
-#webpage <- read_html("https://www.moneycontrol.com/news/india/coronavirus-cases-death-count-state-wise-tally-june-30-latest-news-today-maharashtra-most-affected-5478261.html")
+#webpage <- read_html("")
 
 source("MinistryIndia.R")
 updatetime="July 01, 2020 08:00 hrs"
